9d27259d2f8f2d39554932711b55ffb2bb6e0ef4
Andrei Paraschiv <paraschi@adobe.com>
2023-08-16 21:50:11 +0300

    Update data.js

--- blocks/button/data.js
+++ blocks/button/data.js
@@ -1,4 +1,4 @@
-export const tokenAJO = 'Bearer token from AJO';
+export const tokenAJO = 'Bearer token AJO';
 
 /* ------------------------------  POST  DUPLICATE JOURNEY ----------------------------- */
 export const duplicateJourney = 'https://journey-private.adobe.io/authoring/metadata/resource/duplicate';
