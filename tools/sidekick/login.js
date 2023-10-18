4d4323e9346aea2d5781581b80b65dd7c0b393a7
paraschi <paraschi@adobe.com>
2023-10-18 21:44:11 +0300

    simply impl

--- tools/sidekick/login.js
+++ tools/sidekick/login.js
@@ -1,63 +1,36 @@
-//import { AdobeIMS } from '@identity/imslib';
+
+//  Sign In button
+// document.addEventListener('DOMContentLoaded', function() {
+//   let signInButton = document.querySelector('.button.primary');
+//   signInButton.addEventListener('click', function(event) {
+//     event.preventDefault();
+//     executeLogin();
+//   });
+
 
 window.console.log('File loaded: login.js');
+
 function executeLogin() {
+  window.adobeIMS.signIn();
   window.console.log('Executing login.js');
-const ims = {}
-  const imsConfig = {
+
+  // reader initial logged out experience
+  window.console.log('Initializing IMS');
+}
+
+   window.adobeid = {
     client_id: 'ad13336dbc26475d887a7350c024a2a7',
-    //client-_id: 'franklin-ims-ajo',
-    // client_id: 'franklin-exd-ui-stg',
     scope: 'AdobeID,openid',
     locale: 'en_US',
     environment: 'stg1',
     useLocalStorage: false,
     autoValidateToken: true,
-    modalMode: true,
+    modalMode: false,
     onAccessToken: function (tokenInformation) {
       ims.token = tokenInformation.token;
+      window.console.log('onAccessToken', tokenInformation);
     },
     onReady: async function(appState) {
       console.log('onReady', appState);
-      // if not logged in yet, wait for the modal to send us a message
-      if (!adobeIms.getAccessToken()) {
-        window.addEventListener('message', (ev) => {
-            window.console.log('There is no acess token, waiting for message from modal', ev);
-          if (ev.origin !== 'https://main--franklin-starter--andreialexandruparaschiv.hlx.page') {
-            window.console.log('Wrong origin', ev.origin);
-            return;
-          }
-          
-          // Modal will send back a message on successful login
-          // we reload the palette to the provided URL to force reading the auth cookie
-          try {
-            const url = new URL(ev.data);
-            const usp = new URLSearchParams(url.hash.substring(1));
-            const pathname = usp.get('old_hash');
-            window.console.log('Reloaded palette', pathname);
-            window.location.replace(pathname);
-          } catch (err) {
-            console.log(err);
-          }
-        })
-        return adobeIms.signIn({}, {});
-      }
-        window.console.log('There is an access token, getting profile');
-      ims.profile = await adobeIms.getProfile();
-
-      // Pass the IMS auth down into the react app, and since we are not in the
-      // unified shell context, the runtime doesn't exist
-      // This loads the logged in experience now
-      window.console.log('Loading app', ims);
     },
   };
-
-  // reader initial logged out experience
-    window.console.log('Initializing IMS');
-
-  //const adobeIms = new AdobeIMS(imsConfig);  
-  const adobeIms = window['adobeImsFactory'].createIMSLib(imsConfig);
-  adobeIms.initialize(); 
-}
-
-// document.addEventListener('DOMContentLoaded', executeLogin);
